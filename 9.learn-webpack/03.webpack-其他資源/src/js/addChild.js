1aab71ce34e62690b958d624d9c08d7f1d4e0c3c
Wkb2317 <2576267399@qq.com>
2022-05-26 22:39:35 +0800

    feat: 'webpack资源和插件'

---
+++ 9.learn-webpack/03.webpack-其他資源/src/js/addChild.js
@@ -0,0 +1,9 @@
+import '../css/title.css'
+import '../css/common.less'
+const body = document.querySelector('body')
+
+const element = document.createElement('div')
+element.innerHTML = 'css-webacpk'
+element.className = 'title'
+
+body.appendChild(element)
---
+++ 9.learn-webpack/03.webpack-其他資源/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+}
