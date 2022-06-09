3d8f3fd303be5d368d218ed8733cf7f50cec3da8
Wkb2317 <2576267399@qq.com>
2022-06-09 23:17:53 +0800

    feat:notes

--- 14.learnvue-source/03.Mini_vue实现/createApp.js
+++ 14.learnvue-source/03.Mini_vue实现/createApp.js
@@ -6,20 +6,23 @@ function createApp(rootComponent) {
 
       let oldVNode = null
 
+      // 传入watchEffect中的函数会被执行一次
       watchEffect(function () {
         console.log('被执行')
 
         // 判读是否挂载
         if (!isMounted) {
-          // 挂载
+          // 调用render函数，render函数调用h函数，h函数中用到了data.counter
+          // 搜集依赖
           oldVNode = rootComponent.render()
+          // 挂载
           mount(oldVNode, app)
           isMounted = true
 
           console.log(isMounted)
         } else {
           console.log('patch')
-          // 如果依赖改变，则watchEffect传入的函数重新执行
+          // 如果data.counter改变，则watchEffect传入的函数重新执行
           let newVNode = rootComponent.render()
           // 比较节点
           patch(oldVNode, newVNode)
